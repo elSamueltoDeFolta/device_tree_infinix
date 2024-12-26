86e99c22a85ef0fcf3d381173e01f70ad1d31e25
Ex1skeleton01 <panjulskrebek@gmail.com>
2024-12-26 21:32:21 +0800

    Device Tree First

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/pb_Infinix-X6827.mk
+
+COMMON_LUNCH_CHOICES := \
+    pb_Infinix-X6827-user \
+    pb_Infinix-X6827-userdebug \
+    pb_Infinix-X6827-eng
